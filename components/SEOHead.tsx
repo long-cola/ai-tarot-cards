f954b0826e71960cd1f907fd4bacc277965d9a78
qwgao1994 <gaoqianwen1106@gmail.com>
2025-12-28 22:51:40 +0800

    Use brand icons for favicon and SEO

--- components/SEOHead.tsx
+++ components/SEOHead.tsx
@@ -147,6 +147,7 @@ const SEOHead: React.FC<SEOProps> = ({
 
       {/* 图标 */}
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/icon-32.png" />
       <link rel="icon" type="image/png" sizes="192x192" href="/icon-192.png" />
       <link rel="icon" type="image/png" sizes="512x512" href="/icon-512.png" />
 
